c2f6f5aca8abd9b537fe543b4e271becb4ad9558
Steven Phan <steven.phan009@gmail.com>
2020-03-14 02:54:03 -0700

    initialized with ALU

---
+++ ALU/ALU.srcs/sim_1/new/tb_ALU.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/07/2020 11:13:41 AM
+// Design Name: 
+// Module Name: tb_ALU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_ALU(
+    reg []
+    );
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
