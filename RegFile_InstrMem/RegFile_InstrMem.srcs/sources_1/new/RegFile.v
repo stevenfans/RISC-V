6b0ed07d226a060651e398f816393000379c0410
Steven Phan <steven.phan009@gmail.com>
2020-03-31 00:48:31 -0700

    added registerfile

---
+++ RegFile_InstrMem/RegFile_InstrMem.srcs/sources_1/new/RegFile.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/30/2020 07:16:26 PM
+// Design Name: 
+// Module Name: RegFile
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RegFile(
+    input clk,
+    input reset,
+    input rg_wrt_en,
+    input  [4:0] rg_wrt_addr,
+    input  [4:0] rg_rd_addr1,
+    input  [4:0] rg_rd_addr2,
+    input  [31:0] rg_wrt_data,
+    output [31:0] rg_rd_data1,
+    output [31:0] rg_rd_data2
+    );
+    
+    reg [31:0] register [31:0];
+    integer i; 
+    
+    // If Reset signal we should reset the register file 
+    // (set all registers to 32'h00000000)
+    always @(posedge reset, posedge clk)
+    begin
+        if (reset) begin 
+            for(i=0; i<32; i=i+1)
+                register[i] <= 32'b0;
+        end else begin 
+            register[rg_wrt_addr] <= rg_wrt_data; 
+        end
+    end
+    
+    assign rg_rd_data1 = register[rg_rd_addr1];
+    assign rg_rd_data2 = register[rg_rd_addr2];
+    
+    
+endmodule
