0ef33abca4ccf59b3962dee9b0088a406d610dc0
Steven Phan <steven.phan009@gmail.com>
2020-03-30 19:12:03 -0700

    added 2 to 1 mux

---
+++ RegFile_InstrMem/RegFile_InstrMem.srcs/sources_1/new/MUX21.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:  CSULB
+// Engineer: Steven Phan
+// 
+// Create Date: 03/30/2020 07:10:53 PM
+// Design Name: 
+// Module Name: MUX21
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MUX21(
+    input [31:0] D1,
+    input [31:0] D2,
+    input S,
+    output [31:0] Y
+    );
+    
+    assign Y = S ? D1:D2; 
+endmodule
